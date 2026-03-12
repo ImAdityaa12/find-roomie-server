adf11574b518f045158a7f066fb109617e3d5f3f
ImAdityaa12 <adityagupta1291@gmail.com>
2026-03-12 11:27:44 +0530

    style(user): Standardize imports and formatting in user routes
    - Update

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { requireAuth } from '@/middleware/require-auth.js';
-import { onboardUser, onboardHasRoomUser } from './user.controller.js';
+import { requireAuth } from '@/middleware/require-auth';
+import {
+    onboardUser,
+    onboardHasRoomUser,
+} from '@/modules/user/user.controller.ts';
 
 const router = Router();
-router.post('/v1/onboarding',   requireAuth, onboardHasRoomUser);
+router.post('/v1/onboarding', requireAuth, onboardHasRoomUser);
 router.post('/v2/onboarding', requireAuth, onboardUser);
 
 export default router;
